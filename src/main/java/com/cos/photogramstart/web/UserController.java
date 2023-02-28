2f3916ae492beedf18a893bc7d3905fc551b24dc
khsrla9806 <kimhunsope@naver.com>
2023-02-28 23:46:36 +0900

    feat: Security 태그라이브러리를 사용하여 세션 정보 가져오기 (JSP에서 지원)

--- src/main/java/com/cos/photogramstart/web/UserController.java
+++ src/main/java/com/cos/photogramstart/web/UserController.java
@@ -3,6 +3,7 @@ package com.cos.photogramstart.web;
 import com.cos.photogramstart.config.auth.PrincipalUserDetails;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -16,9 +17,9 @@ public class UserController {
     }
 
     // 사용자 정보 수정페이지로 이동
+    // JSP를 사용할 때는 Model을 사용하지 않고, Security 태그라이브러리를 사용
     @GetMapping("/user/{id}/update")
     public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalUserDetails principal) {
-        System.out.println("세션 정보 : " + principal.getUser().getUsername());
         return "user/update";
     }
 }
--- src/main/webapp/WEB-INF/views/layout/header.jsp
+++ src/main/webapp/WEB-INF/views/layout/header.jsp
@@ -2,6 +2,12 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags"%>
 
+<%-- 인증된 세션 정보에 접근하는 방법 (시큐리티 태그라이브러리) --%>
+<%-- 여기서 설정된 var의 principal은 user가 아닌 UserDetails를 나타냄 --%>
+<sec:authorize access="isAuthenticated()">
+    <sec:authentication property="principal" var="principal" />
+</sec:authorize>
+
 <!DOCTYPE html>
 <html lang="en">
 
