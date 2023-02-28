8d3983dfbe8f32b07b94eef2ee1d4916ab27d397
khsrla9806 <kimhunsope@naver.com>
2023-02-28 18:13:39 +0900

    feat: 회원가입 Dto로 받은 데이터를 User로 만들어 DB에 insert 하는 기능 구현

---
+++ src/main/java/com/cos/photogramstart/service/AuthService.java
@@ -0,0 +1,22 @@
+package com.cos.photogramstart.service;
+
+import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class AuthService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public User signup(User user) {
+        User userEntiy = userRepository.save(user);
+
+        return userEntiy;
+    }
+}
--- src/main/java/com/cos/photogramstart/web/AuthController.java
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -1,17 +1,24 @@
 package com.cos.photogramstart.web;
 
+import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.service.AuthService;
 import com.cos.photogramstart.web.dto.auth.SiginupDto;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class AuthController {
 
     // 받아온 DTO의 데이터가 맞는지 Logger 라이브러리를 사용
     private static final Logger log = LoggerFactory.getLogger(AuthController.class);
+
+    private final AuthService authService;
 
     // 로그인 페이지로 이동
     @GetMapping("/auth/signin")
@@ -28,7 +35,9 @@ public class AuthController {
     // 회원가입 기능
     @PostMapping("/auth/signup")
     public String signup(SiginupDto dto) {
-        log.info(dto.toString());
+        User user = dto.toEntity();
+        User userEntity = authService.signup(user);
+        log.info(userEntity.toString());
         return "auth/signin";
     }
 }
