e500da72e6c0bca11711ff25dc981c9e75bd7107
khsrla9806 <kimhunsope@naver.com>
2023-02-28 23:30:37 +0900

    feat: @AuthenticationPrincipal을 이용하여 세션 유저 가져오기

--- src/main/java/com/cos/photogramstart/web/UserController.java
+++ src/main/java/com/cos/photogramstart/web/UserController.java
@@ -1,5 +1,7 @@
 package com.cos.photogramstart.web;
 
+import com.cos.photogramstart.config.auth.PrincipalUserDetails;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +17,8 @@ public class UserController {
 
     // 사용자 정보 수정페이지로 이동
     @GetMapping("/user/{id}/update")
-    public String update(@PathVariable int id) {
+    public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalUserDetails principal) {
+        System.out.println("세션 정보 : " + principal.getUser().getUsername());
         return "user/update";
     }
 }
