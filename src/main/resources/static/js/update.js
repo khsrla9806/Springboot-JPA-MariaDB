6b155ed87ed6947f3323286835d64f42413ab3fe
khsrla9806 <kimhunsope@naver.com>
2023-03-01 13:01:56 +0900

    feat: form 태그 안에 있는 버튼을 사용할 때 필요한 옵션 (preventDefault)

--- src/main/resources/static/js/update.js
+++ src/main/resources/static/js/update.js
@@ -1,5 +1,6 @@
 // (1) 회원정보 수정
-function update(userId) {
+function update(userId, event) {
+    event.preventDefault(); // form태그의 action경로를 비활성화 시킨다. (form 태그 안에 버튼이 있기 때문에 필요한 옵션이다.)
     let data = $("#profileUpdate").serialize();
 
     $.ajax({
