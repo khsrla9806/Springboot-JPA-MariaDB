59a5d284c19308809bd6126ac4155dd1f49bba8e
khsrla9806 <kimhunsope@naver.com>
2023-03-02 17:14:32 +0900

    feat: User와 Image의 양방향성 매핑을 위한 @OneToMany 적용 (LAZY와 EAGER)

--- src/main/java/com/cos/photogramstart/domain/user/User.java
+++ src/main/java/com/cos/photogramstart/domain/user/User.java
@@ -1,5 +1,6 @@
 package com.cos.photogramstart.domain.user;
 
+import com.cos.photogramstart.domain.image.Image;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Builder
 @AllArgsConstructor
@@ -41,6 +43,17 @@ public class User {
     private String profileImageUrl;
 
     private String role; // 역할
+
+
+    // image와 user의 양방향성 연관관계를 매핑하는 작업
+    // 이 연관관계의 주인은 user가 아니니, user 테이블을 만들 때 image 테이블을 만들지는 않을 거다.
+    // User를 Select할 때, 해당 user와 연관된 image들을 모두 가져올 것이다.
+    // EAGER: User를 가져올 때, 해당 userId로 등록된 모든 images를 가져오겠다.
+    // LAZY: User를 가져올 때, 해당 userId로 등록된 모든 images를 가져오지 않겠다.
+    // 우리는 LAZY를 사용한다. LAZY의 또다른 기능은 getImages()가 작동할 때만 images를 가져올 수 있게 된다.
+    // 이유는 양방향성 매핑을 했기 때문에 user 호출 -> image -> image에 있는 user가 호출됨 -> image 이런식으로 무한적인 참조가 발생
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<Image> images;
 
     private LocalDateTime createDate;
 
