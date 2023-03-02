859e737da5fa0673ac61c604b660ea4f8462313b
khsrla9806 <kimhunsope@naver.com>
2023-03-02 16:09:53 +0900

    feat: Url이 아닌 파일을 요청받기 때문에 Dto를 따로 생성

---
+++ src/main/java/com/cos/photogramstart/web/dto/image/ImageUploadDto.java
@@ -0,0 +1,15 @@
+package com.cos.photogramstart.web.dto.image;
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+public class ImageUploadDto {
+
+    // 원래는 photoImageUrl을 받아야 하지만, 우리가 직접 받는것은 Url이 아닌 파일이다.
+    // MultipartFile로 받아주면 다양한 확장자의 파일을 받을 수 있다.
+    private MultipartFile file;
+
+
+    private String caption;
+}
