b49bac993949643a0badfa523d92a2ccb7c5817d
khsrla9806 <kimhunsope@naver.com>
2023-03-06 21:52:41 +0900

    feat: 스토리에 구독한 사람들의 이미지를 가져오기 위한 Native Query 작성

--- src/main/java/com/cos/photogramstart/domain/image/ImageRepository.java
+++ src/main/java/com/cos/photogramstart/domain/image/ImageRepository.java
@@ -1,8 +1,14 @@
 package com.cos.photogramstart.domain.image;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Integer> {
+
+    @Query(value = "SELECT * FROM image WHERE userID IN(SELECT toUserId FROM subscribe WHERE fromUserId = :principalId);", nativeQuery = true)
+    List<Image> customStory(int principalId);
 }
