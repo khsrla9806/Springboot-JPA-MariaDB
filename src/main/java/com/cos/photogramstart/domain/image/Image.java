a12e84f59dcffef547c744a8ec881b16bc45bfc5
khsrla9806 <kimhunsope@naver.com>
2023-03-07 21:41:21 +0900

    feat: Image에 likes와 양방향성 매핑을 위한 코드 추가 및 무한참조 방지

--- src/main/java/com/cos/photogramstart/domain/image/Image.java
+++ src/main/java/com/cos/photogramstart/domain/image/Image.java
@@ -1,11 +1,13 @@
 package com.cos.photogramstart.domain.image;
 
+import com.cos.photogramstart.domain.likes.Likes;
 import com.cos.photogramstart.domain.user.User;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Builder
 @Getter
@@ -32,6 +34,13 @@ public class Image {
     private User user;
 
     // 이미지 좋아요
+    @OneToMany(mappedBy = "image") // 양방향성 매핑 (image를 가져올 때 likes도 가져오기 위해서)
+    @JsonIgnoreProperties({"image"}) // likes를 호출했을 때, Likes에 있는 image는 다시 호출하지 않겠다. (무한참조 방지)
+    private List<Likes> likes;
+
+    // 이미지 좋아요 여부 상태
+    @Transient // DB에는 해당 컬럼을 만들지 않을 때 사용하는 어노테이션
+    private boolean likeState;
 
     // 이미지 좋아요 카운팅
 
