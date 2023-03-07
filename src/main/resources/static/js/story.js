0c001e2656ce5b1731f14ea6a9ef36b425a518a9
khsrla9806 <kimhunsope@naver.com>
2023-03-07 21:47:04 +0900

    feat: likeState에 따라서 랜더링되는 좋아요 버튼을 다르게 랜더링

--- src/main/resources/static/js/story.js
+++ src/main/resources/static/js/story.js
@@ -45,9 +45,15 @@ function getStoryItem(image) {
             <div class="sl__item__contents">
                 <div class="sl__item__contents__icon">
 
-                    <button>
-                        <i class="fas fa-heart active" id="storyLikeIcon-${image.id}" onclick="toggleLike(${image.id})"></i>
-                    </button>
+                    <button>`;
+
+						if (image.likeState) {
+							item += `<i class="fas fa-heart active" id="storyLikeIcon-${image.id}" onClick="toggleLike(${image.id})"></i>`;
+						} else {
+							item += `<i class="far fa-heart" id="storyLikeIcon-${image.id}" onClick="toggleLike(${image.id})"></i>`;
+						}
+
+			item += `</button>
                 </div>
 
                 <span class="like"><b id="storyLikeCount-${image.id}">3 </b>likes</span>
