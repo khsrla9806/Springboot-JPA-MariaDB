9e3c425013d659cb3f25f5f788f9d93961838fb9
khsrla9806 <kimhunsope@naver.com>
2023-03-08 20:28:28 +0900

    feat: image 요청 시 관련 comment를 얻기 위한 양방향성 매핑 (OrderBy)

--- src/main/java/com/cos/photogramstart/domain/image/Image.java
+++ src/main/java/com/cos/photogramstart/domain/image/Image.java
@@ -1,5 +1,6 @@
 package com.cos.photogramstart.domain.image;
 
+import com.cos.photogramstart.domain.comment.Comment;
 import com.cos.photogramstart.domain.likes.Likes;
 import com.cos.photogramstart.domain.user.User;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -47,6 +48,10 @@ public class Image {
     private Integer likeCount;
 
     // 댓글 정보
+    @OrderBy("id DESC")
+    @JsonIgnoreProperties({"image"})
+    @OneToMany(mappedBy = "image")
+    private List<Comment> comments;
 
     private LocalDateTime createDate;
 
