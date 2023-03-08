7ea3762124ef603cdf2b5370f005ac393d16e4ff
khsrla9806 <kimhunsope@naver.com>
2023-03-08 21:07:27 +0900

    feat: 댓글 유효성 검사 로직 구현

--- src/main/java/com/cos/photogramstart/web/api/CommentApiController.java
+++ src/main/java/com/cos/photogramstart/web/api/CommentApiController.java
@@ -2,6 +2,8 @@ package com.cos.photogramstart.web.api;
 
 import com.cos.photogramstart.config.auth.PrincipalUserDetails;
 import com.cos.photogramstart.domain.comment.Comment;
+import com.cos.photogramstart.handler.ex.CustomApiException;
+import com.cos.photogramstart.handler.ex.CustomValidationApiException;
 import com.cos.photogramstart.service.CommentService;
 import com.cos.photogramstart.web.dto.CMRespDto;
 import com.cos.photogramstart.web.dto.comment.CommentDto;
@@ -9,7 +11,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,7 +25,19 @@ public class CommentApiController {
     private final CommentService commentService;
 
     @PostMapping("/api/comment")
-    public ResponseEntity<?> makeComment(@RequestBody CommentDto dto, @AuthenticationPrincipal PrincipalUserDetails principal) {
+    public ResponseEntity<?> makeComment(@Valid @RequestBody CommentDto dto,
+                                         BindingResult bindingResult,
+                                         @AuthenticationPrincipal PrincipalUserDetails principal) {
+        // 유효성 검사 로직
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+            throw new CustomValidationApiException("유효성 검사 실패", errorMap);
+        }
+
+        // 핵심 로직
         Comment comment = commentService.makeComment(dto.getContent(), dto.getImageId(), principal.getUser().getId());
         return new ResponseEntity<>(new CMRespDto<>(1, "댓글 작성 완료", comment), HttpStatus.CREATED);
     }
--- src/main/java/com/cos/photogramstart/web/dto/comment/CommentDto.java
+++ src/main/java/com/cos/photogramstart/web/dto/comment/CommentDto.java
@@ -3,10 +3,15 @@ package com.cos.photogramstart.web.dto.comment;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class CommentDto {
+    @NotBlank
     private String content;
 
+    @NotNull // Integer, int 타입은 NotBlank 유효성 검사가 불가능하다.
     private int imageId;
 }
