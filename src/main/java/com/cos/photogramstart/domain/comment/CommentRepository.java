b8bb966d3f8d64ff3972398e53504c1874183e91
khsrla9806 <kimhunsope@naver.com>
2023-03-08 18:52:30 +0900

    fix: JpaRepository에 대한 제너릭을 추가

--- src/main/java/com/cos/photogramstart/domain/comment/CommentRepository.java
+++ src/main/java/com/cos/photogramstart/domain/comment/CommentRepository.java
@@ -2,5 +2,5 @@ package com.cos.photogramstart.domain.comment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommentRepository extends JpaRepository {
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
 }
