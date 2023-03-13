37bceefe4cbfae39e4ca5d46579e36774c452aab
khsrla9806 <kimhunsope@naver.com>
2023-03-13 22:32:17 +0900

    fix: 카카오 사용자 정보 데이터 null 값 가져오는 문제 해결

--- src/main/java/com/cos/photogramstart/config/oauth/KakaoInfo.java
+++ src/main/java/com/cos/photogramstart/config/oauth/KakaoInfo.java
@@ -3,13 +3,15 @@ package com.cos.photogramstart.config.oauth;
 import java.util.Map;
 
 public class KakaoInfo extends OAuth2UserInfo{
+    private Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+    private Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
     public KakaoInfo(Map<String, Object> attributes) {
         super(attributes);
     }
 
     @Override
     public String getUsername() {
-        return "Kakao_" + (String) attributes.get("id");
+        return "Kakao_" + attributes.get("id");
     }
 
     @Override
@@ -19,16 +21,16 @@ public class KakaoInfo extends OAuth2UserInfo{
 
     @Override
     public String getName() {
-        return (String) attributes.get("nickname");
+        return (String) profile.get("nickname");
     }
 
     @Override
     public String getEmail() {
-        return (String) attributes.get("email");
+        return (String) kakaoAccount.get("email");
     }
 
     @Override
     public String getImageUrl() {
-        return (String) attributes.get("profile_image");
+        return (String) profile.get("profile_image_url");
     }
 }
