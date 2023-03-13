0a5b25478f3c717b61cd6abfaf6bfbe2c5fdcc25
khsrla9806 <kimhunsope@naver.com>
2023-03-13 22:31:49 +0900

    fix: 네이버 로그인 응답 데이터 null 값 가져오는 문제 해결

--- src/main/java/com/cos/photogramstart/config/oauth/NaverInfo.java
+++ src/main/java/com/cos/photogramstart/config/oauth/NaverInfo.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 public class NaverInfo extends OAuth2UserInfo {
     public NaverInfo(Map<String, Object> attributes) {
-        super(attributes);
+        super((Map<String, Object>)attributes.get("response")); // Kakao는 attributes -> response 안에 데이터가 들어가 있다.
     }
 
     @Override
